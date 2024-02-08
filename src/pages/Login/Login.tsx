50679fbd5502449ad64ade02bd87883125529e0b
lara123-pg <lamorimalvescavalcante@gmail.com>
2024-02-08 00:17:36 -0300

    Validação da tela de login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -7,16 +7,33 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Logo } from "../../components/Logo";
 import { Button } from "../../components/Button";
 
+const loginSchema = z.object({
+    email: z.string().email("Insira um email válido"),
+    password: z.string().min(8, "A senha deve ter no mínimo 8 caracteres")
+})
+
+type loginFormData = z.infer<typeof loginSchema>;
+
 export function Login() {
+    const { register, handleSubmit, formState: { errors } } = useForm<loginFormData>({
+        resolver: zodResolver(loginSchema)
+    })
+
+    function login() {
+        console.log("Login feito.")
+    }
+
     return(
         <div className={styles.container}>
             <Logo />
 
             <main className={styles.forms}>
-                <form>
+                <form onSubmit={handleSubmit(login)}>
                     <div className={styles.inputs}>
-                        <input type="email" placeholder="Email" id={styles.email}/>
-                        <input type="password" placeholder="Senha" id={styles.password}/>
+                        <input type="email" placeholder="Email" id={styles.email} {...register("email")}/>
+                        {errors.email && <span>{errors.email.message}</span>}
+                        <input type="password" placeholder="Senha" id={styles.password} {...register("password")}/>
+                        {errors.password && <span>{errors.password.message}</span>}
                     </div>
 
                     <div className={styles.buttonLink}>
