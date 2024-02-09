141e9a4d2b63ce64525cbeb097d9c226c9cd2362
Evellyn Rodrigues <evellynrocha489@gmail.com>
2024-02-09 01:31:40 -0300

    contador de curtidas, não curtidas e comentários

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 //import { Cadastro } from "./pages/Cadastro";
 //import { Login } from "./pages/Login";
 //import { Explorar } from "./pages/Explorar";
-import { CadastroInicial } from "./pages/CadastroInicial";
+//import { CadastroInicial } from "./pages/CadastroInicial";
+import { Explorar } from "./pages/Explorar";
 
 import "./styles/global.scss";
 
 export function App() {
   return (
     <>
-      <CadastroInicial />
+      <Explorar />
     </>
   )
 }
