5f58ce5f7ed325640663f5a6b1179cb69e52d708
Evellyn Rodrigues <evellynrocha489@gmail.com>
2024-02-10 22:00:17 -0300

    Organização das publicações

--- src/pages/Explorar/Explorar.tsx
+++ src/pages/Explorar/Explorar.tsx
@@ -1,16 +1,29 @@
+import React, { useState } from "react";
 import styles from "./Explorar.module.scss";
-
 import { Menu } from "../../components/Menu";
 import { Publication } from "../../components/Publication";
 
 export function Explorar() {
-    return(
+    const [numPublications, setNumPublications] = useState(2); // Começa com o número padrão de publicações
+
+    const handleMorePublication = () => {
+        setNumPublications(prevNumPublications => prevNumPublications + 2); // Aumenta o número de publicações em 2
+    };
+
+    return (
         <div className={styles.container}>
-            <Menu />
-            <Publication />
-            <Publication />
+            <div className={styles.content}>
+                <Menu />
+                <div className={styles.publicationsContainer}>
+                    {[...Array(numPublications)].map((_, index) => (
+                        <Publication key={index} /> // Renderiza o número atual de publicações
+                    ))}
+                </div>
+            </div>
             <footer className={styles.footer}>
-                More...
+            <p>
+                <span onClick={handleMorePublication}>More...</span>
+            </p>
             </footer>
         </div>
     );
