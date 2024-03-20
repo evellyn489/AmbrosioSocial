14dec4f63e794f6395eec2e74a690af885a452b1
Evellyn Rodrigues <evellynrocha489@gmail.com>
2024-03-20 12:06:20 -0300

    Atualização no axios

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3002'
+    baseURL: 'https://ambrosiosocial-api.onrender.com/'
 })
 
 api.interceptors.request.use(
